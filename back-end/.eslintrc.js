78de2362f018e8d9149d2380b12f908e3d236930
hos101010 <hos101010@naver.com>
2020-06-23 03:16:58 +0900

    :wrench: .env 추가 & eslint 적용
    dotenv 파일 추가
    eslint 적용

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -1,17 +1,9 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'react-app'],
-  plugins: ['react', 'jsx-a11y', 'import'],
-  env: {
-    browser: true,
+  "env": {
+    "node": true,
   },
-  rules: {
-    'linebreak-style': 0,
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx'],
-    }],
-    quotes: ['error', 'single', {
-      allowTemplateLiterals: true,
-    }],
-  },
-};
+  "extends": ["airbnb"],
+  "rules": {
+    "linebreak-style": 0,
+  }
+}
