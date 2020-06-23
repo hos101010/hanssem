0f6f7539907bfeb155a413a27fbf1651682e0ef6
hos101010 <hos101010@naver.com>
2020-06-24 08:56:10 +0900

    :sparkles: 레이아웃을 위해 header와 leftNav 구성
    기능은 없음

---
+++ front-end/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background-color: lightgray;
+    width: 100%;
+    height: 280px;    
+`;
+
+function Header() {
+    return (
+    <Div>
+    </Div>
+  );
+}
+
+export default Header;
---
+++ front-end/src/components/LeftNav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background-color: pink;
+    width: 180px;
+    height: 800px;    
+`;
+
+function LeftNav() {
+    return (
+    <Div>
+    </Div>
+  );
+}
+
+export default LeftNav;
